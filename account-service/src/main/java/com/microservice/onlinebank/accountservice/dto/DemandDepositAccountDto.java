e4acf6ee1096df28d1e228c260df069a1b172319
Syr7-s <isasayar7@github.com>
2021-03-06 00:10:16 +0300

    DemandDepositAccountDto.java and SavingsAccountDto.java was edited.

---
+++ account-service/src/main/java/com/microservice/onlinebank/accountservice/dto/DemandDepositAccountDto.java
@@ -0,0 +1,31 @@
+package com.microservice.onlinebank.accountservice.dto;
+
+
+import com.microservice.onlinebank.accountservice.entity.DemandDepositAccount;
+import com.microservice.onlinebank.accountservice.utility.enums.Currency;
+import lombok.Builder;
+import lombok.Data;
+
+
+import java.time.LocalDate;
+
+
+@Data
+@Builder
+public class DemandDepositAccountDto{
+    private long accountNumber;
+    private String accountIban;
+    private int accountBalance;
+    private Currency accountCurrency;
+    private LocalDate accountCreationDate;
+
+    public DemandDepositAccount toDemandDepositAccount(){
+        return DemandDepositAccount.builder()
+                .accountNumber(this.accountNumber)
+                .accountIban(this.accountIban)
+                .accountBalance(this.accountBalance)
+                .accountCurrency(this.accountCurrency)
+                .accountCreationDate(this.accountCreationDate)
+                .build();
+    }
+}
---
+++ account-service/src/main/java/com/microservice/onlinebank/accountservice/dto/SavingsAccountDto.java
@@ -0,0 +1,36 @@
+package com.microservice.onlinebank.accountservice.dto;
+
+import com.microservice.onlinebank.accountservice.entity.SavingsAccount;
+import com.microservice.onlinebank.accountservice.utility.enums.Currency;
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.LocalDate;
+
+@Data
+@Builder
+public class SavingsAccountDto {
+    private long accountNumber;
+    private String accountIban;
+    private int accountBalance;
+    private Currency accountCurrency;
+    private LocalDate accountCreationDate;
+    private int termTime;
+    private double grossInterestReturn;
+    private double savingsAccountNetGain;
+    private double savingsAccountInterestRate;
+
+    public SavingsAccount toSavingsAccount(){
+        return SavingsAccount.builder()
+                .accountNumber(this.accountNumber)
+                .accountIban(this.accountIban)
+                .accountBalance(this.accountBalance)
+                .accountCurrency(this.accountCurrency)
+                .accountCreationDate(this.accountCreationDate)
+                .termTime(this.termTime)
+                .grossInterestReturn(this.grossInterestReturn)
+                .savingsAccountNetGain(this.savingsAccountNetGain)
+                .savingsAccountInterestRate(this.savingsAccountInterestRate)
+                .build();
+    }
+}
