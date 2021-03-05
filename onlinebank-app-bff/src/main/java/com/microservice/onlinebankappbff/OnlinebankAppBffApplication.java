49be42166ac1e92779b4a0e0891031fbdfd97056
Syr7-s <isasayar7@github.com>
2021-03-05 16:25:23 +0300

    onlinebank-app-bff was created.

---
+++ onlinebank-app-bff/src/main/java/com/microservice/onlinebankappbff/OnlinebankAppBffApplication.java
@@ -0,0 +1,13 @@
+package com.microservice.onlinebankappbff;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlinebankAppBffApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnlinebankAppBffApplication.class, args);
+    }
+
+}
---
+++ onlinebank-app-bff/src/test/java/com/microservice/onlinebankappbff/OnlinebankAppBffApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservice.onlinebankappbff;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlinebankAppBffApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
